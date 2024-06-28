fb129faf5440d12ea07d7b8b9b72bd7d5304504c
Rohan1786 <rp9522203@gmail.com>
2024-06-28 18:40:37 +0530

    change successfully

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+const bcrypt=require('bcrypt')
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-// Set EJS as the view engine
-app.set('view engine', 'ejs');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-// Middleware to serve static files
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route to render index.ejs
-app.get('/', (req, res) => {
-  res.render('index');
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// Start server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
+
+module.exports = app;
