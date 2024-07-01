78d50a1171e7ee2441503979e34398f97f6620ee
sainathPatil09 <sainathpatilpanda@gmail.com>
2024-07-01 12:25:27 +0530

    removed depricated function

--- routes/users.js
+++ routes/users.js
@@ -88,7 +88,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
-mongoose.connect('mongodb://localhost:27017/practisingHack', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb://localhost:27017/practisingHack')//, { useNewUrlParser: true, useUnifiedTopology: true }
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
